e82ec7200e591d912f30d775e2785bf6d42b588d
Rosie Brigham & Sam Johnson <sam.johnson@fundingcircle.com>
2014-04-10 12:53:38 +0100

    First commit and added link for homepage plus header partial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Questions::Application.config.secret_key_base = 'e2c0bf1dd9ee92be0fbc981ceae4810187afee757441ad15bbbe15316a18161443ffc4ca102b1f9b97f2bc0a8e2919f0018e353a6162abbacac03780eb266eb4'
